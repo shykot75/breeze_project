be66305074ddbf49a19ea65b2c472573eadb99af
Goldfinch Dev1001 <goldfinch.dev1001@gmail.com>
2024-04-17 16:57:32 +0600

    migration file created

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username')->nullable();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'user'])->nullable()->default('user');
+            $table->string('role_id')->nullable();
+            $table->tinyInteger('status')->nullable()->default(0);
+            $table->text('address')->nullable();
+            $table->text('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
